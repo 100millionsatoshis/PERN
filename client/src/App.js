9e0d02d94e074ee5fb1f7fedf2bead3bb4f5f264
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2024-02-09 16:55:13 +0100

    protected and provate routes have been created.

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,34 @@ import Dashboard from "./pages/dashboard"
 import Register from "./pages/register"
 import Login from "./pages/login"
 
+const PrivateRoutes = () => {
+  const isAuth = false
+
+  return <> {isAuth ? <Outlet /> : <Navigate to="/login" />}</>
+}
+
+const RestrictedRoutes = () => {
+  const isAuth = false
+
+  return <> {!isAuth ? <Outlet /> : <Navigate to="/dashboard" />}</>
+}
+
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
+
+      <Route element={<PrivateRoutes />}>
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Route>
+
+      <Route element={<RestrictedRoutes />}>
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+      </Route>
+      
+      
     </Routes>
   </BrowserRouter>
   )
